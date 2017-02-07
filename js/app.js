5c7656c7354dc549a6e55e4abafde15fc002760c
Jessica Grinberg <jessgrinberg@gmail.com>
2017-02-06 21:33:15 -0800

    new template portfolio

--- js/app.js
+++ js/app.js
@@ -1,68 +1,7 @@
-// alert("hello");
-
-$(document).ready(function(){
-$('#menuToggle').hide()	
-
-	$('#navTop').click(function(){
-		$('#menuToggle').slideToggle()
-	}) 
-
-})
-
-
-$(document).ready(function(){
-$('#itemToggle').hide()	
-
-	$('#navDetail').click(function(){
-		$('#itemToggle').slideToggle()
-	}) 
-
-})
-
-
-
-
-// <span class="glyphicon glyphicon-plus" id="navDetail" data-toggle="modal" data-target="#myModal" ></span>
-// <div class="modal fade" id="myModal" role="dialog">
-//     <div class="modal-dialog modal-lg">
-//       <div class="modal-content">
-//         <div class="modal-header">
-//           <button type="button" class="close" data-dismiss="modal">&times;</button>
-//           <h4 class="modal-title">Tic-Tac-Toe game built with Angular.js and Firebase </h4>
-//         </div>
-//         <div class="modal-body">
-//           <li>Firebase Application</li>
-//           <li>CSS animation</li>
-//           <li>Responsive , Mobile Optimized</li>
-//          <li> Real-time gameplay on multiple devices</li>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-
-
-	 // <span class="glyphicon glyphicon-plus" id="navDetail"></span> 
- 	// 			<div class ="row" id="itemToggle">
-  //           <div class="row menuItem">Angular JS</div>
-  //           	<div class="row menuItem">Css</div>
-		// 		<div class="row menuItem">Firebase</div>
-  //    			</div> 
-
-
-  	// <i class="fa fa-info" id="navDetail" data-toggle="modal" data-target="#myModal" ></i>
-			// 				<div class="modal fade" id="myModal1" role="dialog">
-   //  							<div class="modal-dialog modal-lg">
-   //   								<div class="modal-content">
-   //     									<div class="modal-header">
-   //        									<button type="button" class="close" data-dismiss="modal">&times;</button>
-   //        									<h4 class="modal-title">Tic-Tac-Toe game built with Angular.js and Firebase </h4>
-   //      								</div>
-   //      								<div class="modal-body">
-   //        									<li>Firebase Application</li>
-   //        									<li>CSS animation</li>
-   //        									<li>Responsive , Mobile Optimized</li>
-   //       									<li> Real-time gameplay on multiple devices</li>
-   //      								</div>
-   //    								</div>
-   //  							</div>
-  	// 						</div>
+//Set active links class
+$(".navFlex a").on("click", function () {
+    $(".navFlex a.active").removeClass("active");
+    $(this).addClass("active");
+    //close responsive navbar when a link is clicked
+    $('#menu-toggle').prop('checked', false); 
+});
